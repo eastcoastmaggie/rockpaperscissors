d68ce6602c50f78c34f80faf83fef15d9824594c
Maggie <maggie.a.palmer@gmail.com>
2022-01-07 22:06:16 -0400

    Add basic game controls in index.html and script.js

---
+++ scripts.js
@@ -0,0 +1,72 @@
+let gameOptions = ['rock', 'paper', 'scissors'];
+
+// Start Game
+function startGame(){
+    for(let i = 0; i<5; i++){
+    console.log("Rock, Paper, Scissors!");
+        let userSelection = userPlay();
+        let computerSelection = computerPlay();
+        let outcome = playRound(userSelection, computerSelection);
+        console.log(outcome);
+    }
+}
+
+// Randomly select a play for computer
+function computerPlay(){
+  let selection = Math.floor(Math.random() * gameOptions.length);
+  console.log("Computer plays: " + gameOptions[selection]);
+  return gameOptions[selection];
+}
+
+// User Selection
+function userPlay(){
+  let selection = prompt("What's your move? 'rock', 'paper', 'scissors': ").toLowerCase();
+  if (!gameOptions.includes(selection)){
+      console.log("That is not an option. Please try again.");
+      selection = userPlay();
+  }
+  console.log("You play: " + selection);
+  return selection;
+}
+
+// Compare the two plays to determine the outcome. Rock beats Scissors, Scissors cuts Paper, Paper covers Rock.
+function playRound(userSelection, computerSelection){
+  let winner = '';
+  if (userSelection == computerSelection){
+    return "Tie Game.";
+  } else {
+      switch (userSelection){
+        case 'rock':
+            if (computerSelection == 'paper'){
+               winner = "Computer wins. Paper covers Rock"; 
+               break;
+            } else {
+                winner = "You win. Rock smashes Scissors!";
+            }
+            break;
+        case 'paper':
+            if (computerSelection == 'scissors'){
+                winner = "Computer wins. Scissors cut Paper!";
+                break;
+            } else {
+                winner = "You win. Paper covers Rock!";
+            }
+            break;
+        case 'scissors':
+            if (computerSelection == 'rock'){
+                winner = "Computer wins. Rock smashes Scissors!";
+                break;
+
+            } else {
+                winner = "You win. Scissors cuts Paper!";
+            }
+            break;
+        default:
+            console.log("Error.");
+        }
+        
+        return winner;
+    }
+}
+
+
